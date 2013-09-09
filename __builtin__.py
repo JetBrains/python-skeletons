4f6a61176f4e1e31e4a85b7783ff1213a98a66e8
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2013-09-09 18:38:26 +0400

    Added skeletons for built-in numeric types

--- __builtin__.py
+++ __builtin__.py
@@ -375,3 +375,798 @@ if sys.version_info < (3,):
             :rtype: xrange[int]
             """
             pass
+
+
+class int(object):
+    """Integer numeric type."""
+
+    def __init__(self, x=None, base=10):
+        """Convert a number or string x to an integer, or return 0 if no
+        arguments are given.
+
+        :type x: object
+        :type base: numbers.Integral
+        """
+        pass
+
+    def __add__(self, y):
+        """Sum of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __sub__(self, y):
+        """Difference of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __mul__(self, y):
+        """Product of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __floordiv__(self, y):
+        """Floored quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __mod__(self, y):
+        """Remainder of x / y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __pow__(self, y, modulo=None):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :type modulo: numbers.Integral | None
+        :rtype: int
+        """
+        pass
+
+    def __lshift__(self, n):
+        """x shifted left by n bits.
+
+         :type n: numbers.Integral
+         :rtype: int
+         """
+        pass
+
+    def __rshift__(self, n):
+        """x shifted right by n bits.
+
+         :type n: numbers.Integral
+         :rtype: int
+         """
+        pass
+
+    def __and__(self, y):
+        """Bitwise and of x and y.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        pass
+
+    def __or__(self, y):
+        """Bitwise or of x and y.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        pass
+
+    def __xor__(self, y):
+        """Bitwise exclusive or of x and y.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        pass
+
+    def __div__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __truediv__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __radd__(self, y):
+        """Sum of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __rsub__(self, y):
+        """Difference of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __rmul__(self, y):
+        """Product of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __rfloordiv__(self, y):
+        """Floored quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __rmod__(self, y):
+        """Remainder of y / x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __rpow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __rlshift__(self, y):
+        """y shifted left by x bits.
+
+         :type y: numbers.Integral
+         :rtype: int
+         """
+        pass
+
+    def __rrshift__(self, y):
+        """y shifted right by n bits.
+
+         :type y: numbers.Integral
+         :rtype: int
+         """
+        pass
+
+    def __rand__(self, y):
+        """Bitwise and of y and x.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        pass
+
+    def __ror__(self, y):
+        """Bitwise or of y and x.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        pass
+
+    def __rxor__(self, y):
+        """Bitwise exclusive or of y and x.
+
+        :type y: numbers.Integral
+        :rtype: int
+        """
+        pass
+
+    def __rdiv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __rtruediv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: int
+        """
+        pass
+
+    def __pos__(self):
+        """x unchanged.
+
+        :rtype: int
+        """
+        pass
+
+    def __neg__(self):
+        """x negated.
+
+        :rtype: int
+        """
+        pass
+
+    def __invert__(self):
+        """The bits of x inverted.
+
+        :rtype: int
+        """
+        pass
+
+
+if sys.version_info < (3,):
+    class long(object):
+        """Long integer numeric type."""
+
+        def __init__(self, x=None, base=10):
+            """Convert a number or string x to a long integer, or return 0 if
+            no arguments are given.
+
+            :type x: object
+            :type base: numbers.Integral
+            """
+            pass
+
+        def __add__(self, y):
+            """Sum of x and y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __sub__(self, y):
+            """Difference of x and y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __mul__(self, y):
+            """Product of x and y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __floordiv__(self, y):
+            """Floored quotient of x and y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __mod__(self, y):
+            """Remainder of x / y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __pow__(self, y, modulo=None):
+            """x to the power y.
+
+            :type y: numbers.Number
+            :type modulo: numbers.Integral | None
+            :rtype: long
+            """
+            pass
+
+        def __lshift__(self, n):
+            """x shifted left by n bits.
+
+             :type n: numbers.Integral
+             :rtype: long
+             """
+            pass
+
+        def __rshift__(self, n):
+            """x shifted right by n bits.
+
+             :type n: numbers.Integral
+             :rtype: long
+             """
+            pass
+
+        def __and__(self, y):
+            """Bitwise and of x and y.
+
+            :type y: numbers.Integral
+            :rtype: long
+            """
+            pass
+
+        def __or__(self, y):
+            """Bitwise or of x and y.
+
+            :type y: numbers.Integral
+            :rtype: long
+            """
+            pass
+
+        def __xor__(self, y):
+            """Bitwise exclusive or of x and y.
+
+            :type y: numbers.Integral
+            :rtype: long
+            """
+            pass
+
+        def __div__(self, y):
+            """Quotient of x and y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __truediv__(self, y):
+            """Quotient of x and y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __radd__(self, y):
+            """Sum of y and x.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __rsub__(self, y):
+            """Difference of y and x.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __rmul__(self, y):
+            """Product of y and x.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __rfloordiv__(self, y):
+            """Floored quotient of y and x.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __rmod__(self, y):
+            """Remainder of y / x.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __rpow__(self, y):
+            """x to the power y.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __rlshift__(self, y):
+            """y shifted left by x bits.
+
+             :type y: numbers.Integral
+             :rtype: long
+             """
+            pass
+
+        def __rrshift__(self, y):
+            """y shifted right by n bits.
+
+             :type y: numbers.Integral
+             :rtype: long
+             """
+            pass
+
+        def __rand__(self, y):
+            """Bitwise and of y and x.
+
+            :type y: numbers.Integral
+            :rtype: long
+            """
+            pass
+
+        def __ror__(self, y):
+            """Bitwise or of y and x.
+
+            :type y: numbers.Integral
+            :rtype: long
+            """
+            pass
+
+        def __rxor__(self, y):
+            """Bitwise exclusive or of y and x.
+
+            :type y: numbers.Integral
+            :rtype: long
+            """
+            pass
+
+        def __rdiv__(self, y):
+            """Quotient of y and x.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __rtruediv__(self, y):
+            """Quotient of y and x.
+
+            :type y: numbers.Number
+            :rtype: long
+            """
+            pass
+
+        def __pos__(self):
+            """x unchanged.
+
+            :rtype: long
+            """
+            pass
+
+        def __neg__(self):
+            """x negated.
+
+            :rtype: long
+            """
+            pass
+
+        def __invert__(self):
+            """The bits of x inverted.
+
+            :rtype: long
+            """
+            pass
+
+
+class float(object):
+    """Floating point numeric type."""
+
+    def __init__(self, x=None):
+        """Convert a string or a number to floating point.
+
+        :type x: object
+        """
+        pass
+
+    def __add__(self, y):
+        """Sum of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __sub__(self, y):
+        """Difference of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __mul__(self, y):
+        """Product of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __floordiv__(self, y):
+        """Floored quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __mod__(self, y):
+        """Remainder of x / y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __pow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __div__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __truediv__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __radd__(self, y):
+        """Sum of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __rsub__(self, y):
+        """Difference of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __rmul__(self, y):
+        """Product of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __rfloordiv__(self, y):
+        """Floored quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __rmod__(self, y):
+        """Remainder of y / x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __rpow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __rdiv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __rtruediv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: float
+        """
+        pass
+
+    def __pos__(self):
+        """x unchanged.
+
+        :rtype: float
+        """
+        pass
+
+    def __neg__(self):
+        """x negated.
+
+        :rtype: float
+        """
+        pass
+
+
+class complex(object):
+    """Complex numeric type."""
+
+    def __init__(self, real=None, imag=None):
+        """Create a complex number with the value real + imag*j or convert a
+        string or number to a complex number.
+
+        :type real: object
+        :type imag: object
+        """
+        pass
+
+    def __add__(self, y):
+        """Sum of x and y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __sub__(self, y):
+        """Difference of x and y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __mul__(self, y):
+        """Product of x and y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __floordiv__(self, y):
+        """Floored quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __mod__(self, y):
+        """Remainder of x / y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __pow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __div__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __truediv__(self, y):
+        """Quotient of x and y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __radd__(self, y):
+        """Sum of y and x.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __rsub__(self, y):
+        """Difference of y and x.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __rmul__(self, y):
+        """Product of y and x.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __rfloordiv__(self, y):
+        """Floored quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __rmod__(self, y):
+        """Remainder of y / x.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __rpow__(self, y):
+        """x to the power y.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __rdiv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __rtruediv__(self, y):
+        """Quotient of y and x.
+
+        :type y: numbers.Number
+        :rtype: complex
+        """
+        pass
+
+    def __pos__(self):
+        """x unchanged.
+
+        :rtype: complex
+        """
+        pass
+
+    def __neg__(self):
+        """x negated.
+
+        :rtype: complex
+        """
+        pass

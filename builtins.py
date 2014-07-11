74b1459c702ec632290bde4da61c74a5388ea6da
Mikhail Golubev <mikhail.golubev@jetbrains.com>
2014-07-11 22:59:09 +0400

    PY-4073 Add completion for special function attributes
    * Introduce new '__function' fake class for functions. Add its skeleton to user skeletons
    and binary skeletons generator. Delegate to it in PyFunctionType for completion variants
    and attributes resolution.
    *  Regenerate test SDKs to use new class.

--- builtins.py
+++ builtins.py
@@ -2031,3 +2031,23 @@ class __generator(object):
         :rtype: None
         """
         pass
+
+class __function(object):
+    """A mock class representing function type."""
+
+    def __init__(self):
+        self.__name__ = ''
+        self.__doc__ = ''
+        self.__dict__ = ''
+        self.__module__ = ''
+
+        self.__annotations__ = {}
+        self.__defaults__ = {}
+        self.__globals__ = {}
+        self.__kwdefaults__ = {}
+        self.__closure__ = None
+        self.__code__ = None
+
+        if sys.version_info >= (3, 3):
+            self.__qualname__ = ''
+

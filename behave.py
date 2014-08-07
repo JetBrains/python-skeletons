8853f6c1d94e46465e8a24a66bb96d41c6d251af
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2014-08-08 00:32:27 +0400

    PY-13641 Behave: unresolved reference: false positive for uppercase step definition decorator

--- behave.py
+++ behave.py
@@ -31,3 +31,30 @@ def step(pattern):
     :param pattern pattern to match, may be regular expression or something else depends on step matcher
     """
     pass
+def Given(pattern):
+    """Decorates a function, so that it will become a new step
+    definition.
+    :param pattern pattern to match, may be regular expression or something else depends on step matcher
+    """
+    pass
+
+def When(pattern):
+    """Decorates a function, so that it will become a new step
+    definition.
+    :param pattern pattern to match, may be regular expression or something else depends on step matcher
+    """
+    pass
+
+def Then(pattern):
+    """Decorates a function, so that it will become a new step
+    definition.
+    :param pattern pattern to match, may be regular expression or something else depends on step matcher
+    """
+    pass
+
+def Step(pattern):
+    """Decorates a function, so that it will become a new step
+    definition.
+    :param pattern pattern to match, may be regular expression or something else depends on step matcher
+    """
+    pass

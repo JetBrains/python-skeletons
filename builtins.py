031d9cc79fed5a01f0bbb6a176b9eb2fbd55786c
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2014-09-19 17:44:20 +0400

    Moved reduce() skeleton from builtins to functools for Python 3

--- builtins.py
+++ builtins.py
@@ -275,19 +275,6 @@ class range(object):
         pass
 
 
-def reduce(function, sequence, initial=None):
-    """Apply a function of two arguments cumulatively to the items of a
-    sequence, from left to right, so as to reduce the sequence to a single
-    value.
-
-    :type function: collections.Callable
-    :type sequence: collections.Iterable
-    :type initial: T
-    :rtype: T | unknown
-    """
-    return initial
-
-
 def repr(object):
     """
     Return the canonical string representation of the object.

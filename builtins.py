d8be0ac82a1fdefe4e817c2657dd76f6751af77c
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2015-09-15 14:28:05 +0300

    Skeleton for coroutine type

--- builtins.py
+++ builtins.py
@@ -2053,6 +2053,51 @@ class __generator(object):
         pass
 
 
+class __coroutine(object):
+    """A mock class representing the generator function type."""
+
+    def __init__(self):
+        """
+        :rtype: __coroutine[V]
+        """
+        self.__name__ = ''
+        self.__qualname__ = ''
+        self.cr_await = None
+        self.cr_frame = None
+        self.cr_running = False
+        self.cr_code = None
+
+    def __await__(self):
+        """
+        :rtype: __generator[unknown, unknown, V]
+        """
+        return []
+
+    def __iter__(self):
+        """
+        :rtype: collections.Iterable[unknown]
+        """
+        return []
+
+    def close(self):
+        """
+        :rtype: None
+        """
+        pass
+
+    def send(self, value):
+        """
+        :rtype: None
+        """
+        pass
+
+    def throw(self, type, value=None, traceback=None):
+        """
+        :rtype: None
+        """
+        pass
+
+
 class __function(object):
     """A mock class representing function type."""
 

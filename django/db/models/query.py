91b1a4a0eee909546012be55a709e0d6c7ffcc9b
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2015-09-15 17:36:39 +0300

    QuerySet should be parametrized with model object type to return iterator of it

---
+++ django/db/models/query.py
@@ -0,0 +1,23 @@
+"""
+QuerySet is parametrized with model object
+"""
+class QuerySet(object):
+
+    def __init__(self):
+        """
+        :rtype: collections.Iterator[T]
+        """
+        pass
+
+    def __iter__(self):
+        """
+
+        :rtype: list of T
+        """
+        pass
+
+    def __next__(self):
+        """
+        :rtype: T
+        """
+        pass

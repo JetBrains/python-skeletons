9bdba761f0a99a3f490bf5a6eed67edeb6c63b81
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2015-09-15 21:15:24 +0300

    QuerySet paramerized with T and is iterable with T

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -2,22 +2,15 @@
 QuerySet is parametrized with model object
 """
 class QuerySet(object):
-
     def __init__(self):
         """
-        :rtype: collections.Iterator[T]
+        :rtype: QuerySet[T]
         """
         pass
 
     def __iter__(self):
         """
 
-        :rtype: list of T
-        """
-        pass
-
-    def __next__(self):
-        """
-        :rtype: T
+        :rtype: collections.Iterator[T]
         """
         pass
